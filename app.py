6c27e269be38e50b1237820b2068c2a964c718b1
Shrey-Sheladia <shrey.sheladia@gmail.com>
2023-05-04 10:05:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,7 @@ pdf = st.file_uploader("Upload your PDF", type="pdf")
 
 if pdf is not None:
     if 'knowledge_base' not in st.session_state or str(st.session_state.pdf) != str(pdf):
-        print(str(st.session_state.pdf) == str(pdf) if "pdf" in st.session_state else print("No PDF"))
-        print(st.session_state.pdf,pdf, sep = "\n")
-        print(st.session_state)
+
         # Create Knowledge base
         st.session_state.pdf = pdf
         st.session_state.knowledge_base = create_knowledge_base_from_pdf(pdf)
