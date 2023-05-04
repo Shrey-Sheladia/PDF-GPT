dd3a399fba5a6dc2efc1dab37b665b82a12813da
Shrey-Sheladia <shrey.sheladia@gmail.com>
2023-05-04 09:50:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,43 @@
-from dotenv import load_dotenv
+try:
+    from dotenv import load_dotenv
+except:
+    pass
 import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
 from Utils import *
 
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
 
-def main():
-    load_dotenv()
-    st.set_page_config(page_title="Ask your PDF")
-    st.header("Ask your PDF 💬")
-    
-    # upload file
-    pdf = st.file_uploader("Upload your PDF", type="pdf")
+load_dotenv()
+st.set_page_config(page_title="Ask your PDF")
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-    if pdf is not None:
-        if 'knowledge_base' not in st.session_state or str(st.session_state.pdf) != str(pdf):
-            print(str(st.session_state.pdf) == str(pdf) if "pdf" in st.session_state else print("No PDF"))
-            print(st.session_state.pdf,pdf, sep = "\n")
-            print(st.session_state)
-            # Create Knowledge base
-            st.session_state.pdf = pdf
-            st.session_state.knowledge_base = create_knowledge_base_from_pdf(pdf)
-        
+st.header("Ask your PDF 💬")
 
-        answer = st.empty()
-        # show user input
-        user_question = st.text_input("Ask a question about your PDF:")
-        if user_question:
+# upload file
+pdf = st.file_uploader("Upload your PDF", type="pdf")
 
-            response, cb = get_response(user_question, st.session_state.knowledge_base)
-            print(cb)
-                
-            answer.write(response)
+if pdf is not None:
+    if 'knowledge_base' not in st.session_state or str(st.session_state.pdf) != str(pdf):
+        print(str(st.session_state.pdf) == str(pdf) if "pdf" in st.session_state else print("No PDF"))
+        print(st.session_state.pdf,pdf, sep = "\n")
+        print(st.session_state)
+        # Create Knowledge base
+        st.session_state.pdf = pdf
+        st.session_state.knowledge_base = create_knowledge_base_from_pdf(pdf)
     
 
-if __name__ == '__main__':
-    main()
+    answer = st.empty()
+    # show user input
+    user_question = st.text_input("Ask a question about your PDF:")
+    if user_question:
+
+        response, cb = get_response(user_question, st.session_state.knowledge_base)
+        print(cb)
+            
+        answer.write(response)
+    
