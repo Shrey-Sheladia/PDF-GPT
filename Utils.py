2c45393458c44ec0126102104781a91c6a1b0c5e
Shrey-Sheladia <shrey.sheladia@gmail.com>
2023-05-09 17:18:37 -0700

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -10,6 +10,7 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
+import faiss
 
 load_dotenv()
 def get_chunks(pdf_file):
@@ -30,7 +31,7 @@ def get_chunks(pdf_file):
     chunks = text_splitter.split_text(text)
     print("Created Chunks")
     return chunks
-
+ 
 
 def create_knowledge_base_from_pdf(pdf):
     print("Creating Knowledge Base")
@@ -56,6 +57,27 @@ def get_response(user_question, knowledge_base):
 
         print("Got Respnse")
         return response, cb
+
+
+def merge_faiss_indices_from_pdf(pdf_path, existing_index_path):
+    # Load existing index
+    existing_index = faiss.read_index(existing_index_path)
+
+    # Create new index from PDF
+    chunks = get_chunks(pdf_path)
+    embeddings = OpenAIEmbeddings()
+    new_index = FAISS.from_texts(chunks, embeddings)
+
+    # Merge the new index into the existing index
+    combined_index = faiss.IndexFlatIP(existing_index.d + new_index.d)
+    faiss.merge_into_lattice(existing_index, new_index, combined_index)
+
+    # Save the combined index to disk
+    faiss.write_index(combined_index, existing_index_path)
+
+    # Return the combined index
+    return combined_index
+
     
 
 
